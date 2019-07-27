00c776364c9de15451ff4bfb5d2eef00e999b949
sbj1811 <sbj1811@.github.com>
2019-07-27 13:46:00 -0700

    Added: Branch Android app links Fixed: Branch URI and NPGApplication class

--- app/src/main/java/com/sjani/usnationalparkguide/NPGApplication.java
+++ app/src/main/java/com/sjani/usnationalparkguide/NPGApplication.java
@@ -1,14 +1,34 @@
 package com.sjani.usnationalparkguide;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import io.branch.referral.Branch;
 
 public class NPGApplication extends Application {
 
+    private RefWatcher refWatcher;
+
+    public static RefWatcher getRefWatcher(Context context) {
+        NPGApplication application = (NPGApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        refWatcher = LeakCanary.install(this);
+        // Normal app init code...
         Branch.getAutoInstance(this);
     }
+
+
 }
